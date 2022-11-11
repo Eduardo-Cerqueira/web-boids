c95ff2600bb75151a0719bb617e47ee42890d3ef
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2022-11-11 03:06:56 +0100

    vite setup

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Start using Three.js !</h1>
+  </div>
+`
